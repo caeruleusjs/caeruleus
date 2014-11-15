b29713239f1c6b9adf5cb3776dc2a10e60c4b15e
Michael F <tehfreak@tehfreak.com>
2014-11-15 05:08:04 +0200

    добавил сценарий сборки

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+var gulp= require('gulp')
+
+var gulpLess= require('gulp-less')
+var gulpJade= require('gulp-jade')
+
+
+
+gulp.task('scripts', function () {
+    return gulp.src(['browser/source/scripts/**/*.js'])
+        .pipe(gulp.dest('browser/release/scripts'))
+    ;
+})
+
+gulp.task('styles', function () {
+    return gulp.src(['browser/source/styles/**/*.less'])
+        .pipe(gulpLess())
+        .pipe(gulp.dest('browser/release/styles'))
+    ;
+})
+
+gulp.task('templates', function() {
+    return gulp.src('browser/source/**/*.jade')
+        .pipe(gulpJade({ locals: {}, pretty: true }))
+        .pipe(gulp.dest('browser/release'))
+    ;
+})
+
+gulp.task('watch', function () {
+    gulp.watch(['browser/source/scripts/**/*.js'], ['scripts'])
+    gulp.watch(['browser/source/styles/**/*.less'], ['styles'])
+    gulp.watch(['browser/source/**/*.jade'], ['templates'])
+})
+
+
+
+gulp.task('default', ['scripts', 'styles', 'templates', 'watch'])
